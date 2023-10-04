d9391792f1ea7bfc30cffa877669b7689bc66e84
g1ftmkyelu <mkyelugift@gmail.com>
2023-10-04 06:20:49 +0200

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,59 @@ import React from 'react';
 import { DynamicForm, HeroSection } from '../components';
 import { Container } from 'react-bootstrap';
 import { ContactFormData } from '../assets/data/data';
+import { useAddmessage } from '../hooks/messageAPI';
+import Swal from 'sweetalert2';
+
 
 const Contact = () => {
-    const {ButtonText,Fields,Title}=ContactFormData
+    const { ButtonText, Fields, Title } = ContactFormData;
+
+    // Define a mutation hook for adding a new message
+    const addMessageMutation = useAddmessage();
+
+    const handleSubmit = async (formData) => {
+        try {
+            // Show a confirmation dialog to the user
+            const result = await Swal.fire({
+                title: 'Confirm Message Submission',
+                text: 'Are you sure you want to submit this message?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, submit it!',
+                cancelButtonText: 'Cancel',
+            });
+    
+            if (result.isConfirmed) {
+                // Send the new message data to the API
+                await addMessageMutation.mutateAsync(formData);
+    
+                // Show a success message to the user
+                Swal.fire({
+                    title: 'Success',
+                    text: 'Message submitted successfully',
+                    icon: 'success',
+                });
+            } else {
+                // The user canceled the submission
+                Swal.fire({
+                    title: 'Cancelled',
+                    text: 'Message submission was cancelled',
+                    icon: 'info',
+                });
+            }
+        } catch (error) {
+            // Handle API request error
+            console.error(error);
+            // Show an error message to the user
+            Swal.fire({
+                title: 'Error',
+                text: 'Error submitting message. Please try again later.',
+                icon: 'error',
+            });
+        }
+    };
+    
+
     return (
         <>
             <HeroSection
@@ -15,9 +65,8 @@ const Contact = () => {
                 <DynamicForm
                     title={Title}
                     formFields={Fields}
-                    buttonText={ButtonText}                    
-                    onSubmit={()=>alert('submited')}
-                
+                    buttonText={ButtonText}
+                    onSubmit={handleSubmit} // Use the handleSubmit function for form submission
                 />
             </Container>
         </>
