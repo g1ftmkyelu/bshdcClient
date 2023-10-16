9877becd7d15eb97d4a1b7fb0add474b74527fb7
g1ftmkyelu <mkyelugift@gmail.com>
2023-10-16 07:02:16 +0200

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,26 @@
 
-import React, { useState } from 'react';
 import { Navbar, Container, Nav, Image, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const logoImageUrl = 'https://www.malawiyp.com/img/mw/r/1624560523-97-ccap-blantyre-synod.jpg';
 
 function NavBar({ Links }) {
-
-    const [menuOpen, setMenuOpen] = useState(false);
-    const toggleMenu = () => setMenuOpen(!menuOpen);
-
-
     return (
-        <Navbar bg="white" expand="lg" expanded={menuOpen}>
+        <Navbar bg="white" expand="lg">
             <Container>
                 <Navbar.Brand href="/">
                     <Image src={logoImageUrl} width={100} height={100} />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" onSelect={toggleMenu}>
+                    <Nav className="me-auto">
+
                         {Links.map(({ Caption, url }, i) => (
                             <Link key={i} to={url} className='px-2 py-2 text-decoration-none text-body'>
                                 {Caption}
                             </Link>
                         ))}
                     </Nav>
-
                 </Navbar.Collapse>
             </Container>
 
